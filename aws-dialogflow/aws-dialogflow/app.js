9aa20f0a4890f07acd573d02c0822ced421730ad
Glenn Wedin <glenn.wedin@gmail.com>
2019-04-28 04:23:28 +0200

    Readme updates

--- aws-dialogflow/aws-dialogflow/app.js
+++ aws-dialogflow/aws-dialogflow/app.js
@@ -1,9 +1,3 @@
-// # Package SAM template
-// sam package --template-file template.yaml --s3-bucket aws-dialogflowbucket --output-template-file packaged.yaml
-
-// # Deploy packaged SAM template
-// sam deploy --template-file ./packaged.yaml --stack-name aws-dialogflow --capabilities CAPABILITY_IAM
-
 const { dialogflow } = require('actions-on-google');
 const app = dialogflow({ debug: true });
 
